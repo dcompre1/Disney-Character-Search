74364b9d9407d9c04966f2f05dd433a55652093b
dcompre1 <dcompre1@jh.edu>
2022-06-29 17:32:10 -0400

    updates to disney

--- disney.py
+++ disney.py
@@ -4,8 +4,14 @@ url = "https://api.disneyapi.dev/characters"
 response = requests.get(url)
 data = response.json()
 
-print(data['data'][1]['tvShows'])
-url2 = data['data'][1]['url']
-
-print(url2)
-
+for i in range(10):
+    film = data['data'][i]['films']
+    tv = data['data'][i]['tvShows']
+    if len(tv) == 0:
+        continue
+    else:
+        print(str(data['data'][i]['name']) + " appears in " + str(tv))
+    if len(film) == 0:
+        continue
+    else:
+        print(str(data['data'][i]['name']) + " appears in " + str(film))
