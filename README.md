538f35c1c11e14250759764545bed5a20509ffab
Dinamary Compres <dinmarycompres@gmail.com>
2022-06-30 15:05:20 +0000

    added some lines trying to make a database, curretly just prints entire API

---
+++ README.md
@@ -0,0 +1,4 @@
+Dinamary Compres 
+SEO Week 1 Project 
+All you need to do is git clone the repository
+
--- disney.py
+++ disney.py
@@ -1,9 +1,21 @@
 import requests
+#import sqlalchemy as db
+#import pd
 
 url = "https://api.disneyapi.dev/characters"
 response = requests.get(url)
 data = response.json()
 
+print(data)
+#pd.DataFrame.from_dict(data)
+
+#engine = db.create_engine('sqlite:///data_base_name.db')
+#dataframe_name.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+#query_result = engine.execute("SELECT * FROM table;").fetchall()
+#print(pd.DataFrame(query_result))
+
+'''
 for i in range(10):
     film = data['data'][i]['films']
     tv = data['data'][i]['tvShows']
@@ -15,3 +27,4 @@ for i in range(10):
         continue
     else:
         print(str(data['data'][i]['name']) + " appears in " + str(film))
+'''
