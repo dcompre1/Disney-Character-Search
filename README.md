9f7bd8511871235702be30a5e8d9581668d71317
Dinamary Compres <dinmarycompres@gmail.com>
2022-06-30 19:21:25 +0000

    lots of improvements, code works but is not formatted in functions, TODO comments updated, README updated

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 Dinamary Compres 
 SEO Week 1 Project 
-All you need to do is git clone the repository
+git repository: https://github.com/dcompre1/SEO_Proj_1
+
+This is a program that takes in user input of a Disney character name and then outputs information about the character
+This is done by accessing Disney's API, creating a database for information about each Disney Character, then querying that database
+
+imported libraries:
+- pandas
+- requests
+- sqlalchemy 
+
+pycodestyle style guide is used and a workflow is connected to this repository that will run pycodestyle on every git action
+ 
 
--- disney.py
+++ disney.py
@@ -2,11 +2,11 @@ import requests
 import sqlalchemy as db
 import pandas as pd
 
-#USER CAN ENTER A CHARACTERS NAME AND RECEIVE ALL THE INFO ABOUT THE CHARACTER
-#TODO: MODULARIZE DIFF PARTS LIKE DATABASE STUFF, USER INPUT, PRINT OUTPUT, GET DATA CAN ALL BE FUNCTIONS DUDE 
-#character_name = input("Please enter a character name: ")
-#TODO: VALIDATE USER INPUT, MAKES SURE ITS A STRING
+character_name = input("Please enter a character name: ")
+while character_name.strip().isdigit():
+	character_name = input("That is not a valid character name, please enter a new name:")
 
+#TODO: consider making these requests into a separate function
 url = "https://api.disneyapi.dev/characters"
 response = requests.get(url)
 data = response.json()['data']
@@ -21,30 +21,36 @@ for i in range(pages - 2):
 #reorganize data into a dictionary 
 new_data = {}
 #make values lists, put category names into database as columns
-for i in range(7):
+#TODO: consider making a function for this loop
+exists = False
+for i in range(pages):
 	entry = data[i]
-	films = ' '.join(entry['films'])
-	shortFilms = ' '.join(entry['shortFilms'])
-	tvShows = ' '.join(entry['tvShows'])
-	videoGames = ' '.join(entry['videoGames'])
-	parkAttractions = ' '.join(entry['parkAttractions'])
-	allies = ' '.join(entry['allies'])
-	enemies = ' '.join(entry['enemies'])
 	name = entry['name']
-	del entry['url']
-	del entry['imageUrl']
-	del entry['_id']
-	del entry['name']
-	new_data[name] = [films, shortFilms, tvShows, videoGames, parkAttractions, allies, enemies]
+	if exists == False and name == character_name:
+		exists = True
+	films = '\n'.join(entry['films'])
+	shortFilms = '\n'.join(entry['shortFilms'])
+	tvShows = '\n'.join(entry['tvShows'])
+	videoGames = '\n'.join(entry['videoGames'])
+	new_data[i] = [name, films, shortFilms, tvShows, videoGames]
 
-print(new_data)
+if exists == False:
+	print("The name you entered is not a Disney Character")
+else:
+	#TODO: make this into a function to create_database
+	disneydf = pd.DataFrame.from_dict(new_data, orient='index', columns=['name', 'films', 'shortFilms', 'tvShows', 'videoGames'])
 
-#TODO: make this into a function to create_database
-disneydf = pd.DataFrame.from_dict(new_data, orient='index', columns=['films', 'shortFilms', 'tvShows', 'videoGames', 'parkAttractions', 'allies', 'enemies'])
+	engine = db.create_engine('sqlite:///Disney.db')
+	disneydf.to_sql('disney_info', con=engine, if_exists='replace', index=False)
 
-engine = db.create_engine('sqlite:///Disney.db')
-disneydf.to_sql('disney_info', con=engine, if_exists='replace', index=False)
+	#TODO: make a function to retrieve this data from database
+	film_result = engine.execute("SELECT films FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
+	s_film_result = engine.execute("SELECT shortFilms FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
+	tv_result = engine.execute("SELECT tvShows FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
+	game_result = engine.execute("SELECT videoGames FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
 
-query_result = engine.execute("SELECT * FROM disney_info;").fetchall()
-#print(pd.DataFrame(query_result))
-
+	#TODO: make a print function to do this 
+	print(character_name + " appears in these films:\n" + pd.DataFrame(film_result)[0][0] + "\n")
+	print(character_name + " appears in these short films:\n" + pd.DataFrame(s_film_result)[0][0] + "\n")
+	print(character_name + " appears in these tv shows:\n" + pd.DataFrame(tv_result)[0][0] + "\n")
+	print(character_name + " appears in these video games:\n" + pd.DataFrame(game_result)[0][0] + "\n")
