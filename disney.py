73805381871c43693a8710bcd0f5028b7e37fd92
Dinamary Compres <dinmarycompres@gmail.com>
2022-07-01 04:45:52 +0000

    checkstyle compliant, test file should be added and README updated

--- disney.py
+++ disney.py
@@ -4,32 +4,39 @@ import pandas as pd
 
 character_name = input("Please enter a character name: ")
 while character_name.strip().isdigit():
-    character_name = input("That is not a valid character name\
-	, please enter a new name:")
+    character_name = input("That is not a valid character name" +
+                           ", please enter a new name:")
 
-#request to API
-req_result = disney_funcs.make_get_request("https://api.disneyapi.dev/characters")
+# request to API
+req_result = disney_funcs.make_get_request("https://api.disneyapi" +
+                                           ".dev/characters")
 data = req_result[0]
 pages = req_result[1]
 
-#make dictionary of Disney data
+# make dictionary of Disney data
 dict_result = disney_funcs.make_dict(character_name, data, pages)
 exists = dict_result[1]
 new_data = dict_result[0]
 
-if exists is False:
-	print("The name you entered is not a Disney Character")
+if exists == 0:
+    print("The name you entered is not a Disney Character")
 else:
-	disneydf = pd.DataFrame.from_dict(new_data, orient='index', columns=['name', 'films', 'shortFilms', 'tvShows', 'videoGames'])
-	engine = db.create_engine('sqlite:///Disney.db')
-	disneydf.to_sql('disney_info', con=engine, if_exists='replace', index=False)
+    disneydf = pd.DataFrame.from_dict(new_data, orient='index',
+                                      columns=['name', 'films',
+                                               'shortFilms', 'tvShows',
+                                               'videoGames'])
+    engine = db.create_engine('sqlite:///Disney.db')
+    disneydf.to_sql('disney_info', con=engine,
+                    if_exists='replace', index=False)
 
-	#getting character information from database
-	character_info = disney_funcs.retrieve_from_database(engine, character_name)
-	films = character_info[0]
-	short_films = character_info[1]
-	tv_shows = character_info[2]
-	video_games = character_info[3]
+    # getting character information from database
+    character_info = disney_funcs.retrieve_from_database(engine,
+                                                         character_name)
+    films = character_info[0]
+    short_films = character_info[1]
+    tv_shows = character_info[2]
+    video_games = character_info[3]
 
-	#print information about character
-	disney_funcs.print_info(character_name, films, short_films, tv_shows, video_games)
+    # print information about character
+    disney_funcs.print_info(character_name, films,
+                            short_films, tv_shows, video_games)
--- disney_funcs.py
+++ disney_funcs.py
@@ -9,21 +9,24 @@ def make_get_request(url):
     pages = response.json()['totalPages']
     nextUrl = response.json()['nextPage']
 
-    for i in range(pages - 2):
+    for i in range(148):
         response = requests.get(nextUrl)
         data = data + response.json()['data']
+        if i == 147:
+            break
         nextUrl = str(response.json()['nextPage'])
     return [data, pages]
+
 
 def make_dict(character_name, data, pages):
     '''organize Disney data into a dictionary and find if character exists'''
     new_data = {}
-    exists = False
-    for i in range(pages):
+    exists = 0
+    for i in range(len(data)):
         entry = data[i]
         name = entry['name']
-        if exists == False and name == character_name:
-            exists = True
+        if exists == 0 and name == character_name:
+            exists = 1
         films = '\n'.join(entry['films'])
         shortFilms = '\n'.join(entry['shortFilms'])
         tvShows = '\n'.join(entry['tvShows'])
@@ -32,17 +35,29 @@ def make_dict(character_name, data, pages):
 
     return [new_data, exists]
 
+
 def print_info(character_name, films, short_films, tv_shows, games):
     '''print character information'''
     print(character_name + " appears in these films:\n" + films + "\n")
-    print(character_name + " appears in these short films:\n" + short_films + "\n")
+    print(character_name + " appears in these short films:\n" +
+          short_films + "\n")
     print(character_name + " appears in these tv shows:\n" + tv_shows + "\n")
     print(character_name + " appears in these video games:\n" + games + "\n")
 
+
 def retrieve_from_database(engine, character_name):
     '''queries to database'''
-    film_result = engine.execute("SELECT films FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
-    s_film_result = engine.execute("SELECT shortFilms FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
-    tv_result = engine.execute("SELECT tvShows FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
-    game_result = engine.execute("SELECT videoGames FROM disney_info WHERE name = \'" + character_name + "\';").fetchall()
-    return [pd.DataFrame(film_result)[0][0], pd.DataFrame(s_film_result)[0][0], pd.DataFrame(tv_result)[0][0], pd.DataFrame(game_result)[0][0]]
+    film_result = engine.execute("SELECT films FROM disney_info WHERE" +
+                                 " name = \'" + character_name +
+                                 "\';").fetchall()
+    s_film_result = engine.execute("SELECT shortFilms FROM disney_info WHERE" +
+                                   " name = \'" + character_name +
+                                   "\';").fetchall()
+    tv_result = engine.execute("SELECT tvShows FROM disney_info WHERE" +
+                               " name = \'" + character_name +
+                               "\';").fetchall()
+    game_result = engine.execute("SELECT videoGames FROM disney_info WHERE" +
+                                 " name = \'" + character_name +
+                                 "\';").fetchall()
+    return [pd.DataFrame(film_result)[0][0], pd.DataFrame(s_film_result)[0][0],
+            pd.DataFrame(tv_result)[0][0], pd.DataFrame(game_result)[0][0]]
